2cfedae0950fbccf77ffdb095526ad773b9a6634
Miszo <miszo97@MacBook-Air-Klaudia.local>
2018-01-04 20:11:03 +0100

    Grouping headers and source files

--- SpaceRouge/MeteorExplosion.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  MeteorExplosion.cpp
-//  SpaceRouge
-//
-//  Created by Miszo on 24.11.2017.
-//  Copyright © 2017 Miszo. All rights reserved.
-//
-
-#include "MeteorExplosion.hpp"
--- SpaceRouge/cScreen.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  cScreen.cpp
-//  SpaceRouge
-//
-//  Created by Miszo on 02.11.2017.
-//  Copyright © 2017 Miszo. All rights reserved.
-//
-
-#include "cScreen.hpp"
